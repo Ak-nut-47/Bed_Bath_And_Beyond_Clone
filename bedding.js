a44e751492d8ff555059813f536295cdc76bf7bb
Animesh Kalita <animesh.kalita22@gmail.com>
2023-01-19 23:32:27 +0530

    day3 kitchen appliances markup and static page ready

---
+++ bedding.js
@@ -0,0 +1,5 @@
+let kitchen_appliances = document.getElementById("kitchen_appliances");
+kitchen_appliances.addEventListener("click", function() {
+    window.location.href = "small_kitchen_appliances.html";
+    console.log("working")
+});
