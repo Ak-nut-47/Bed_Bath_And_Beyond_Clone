748fe671a85af530a2f47cd43e5753f0722537a9
gouravsahuu <gsgourav.sahu@gmail.com>
2023-01-22 22:38:01 +0530

    update

--- adminCustomer.js
+++ adminCustomer.js
@@ -1,6 +1,13 @@
 let dashboard = document.getElementById("dashboard-page");
 let products = document.getElementById("products-page");
 let orders = document.getElementById("orders-page");
+let home = document.getElementById("home");
+let tableBody = document.getElementById("tbody");
+let i = 1;
+
+home.addEventListener("click",() => {
+    window.location = "./index.html";
+})
 
 dashboard.addEventListener("click",() => {
     window.location = "./dashboard.html";
@@ -11,3 +18,42 @@ products.addEventListener("click",() => {
 orders.addEventListener("click",() => {
     window.location = "./adminOrders.html";
 })
+
+window.addEventListener("load",() => {
+     let userData = JSON.parse(localStorage.getItem("user"));
+
+     if(userData == undefined){
+        userData = [];
+     }
+
+     renderCustomers(userData);
+})
+
+function renderCustomers(userData){
+
+    tableBody.innerHTML = "";
+      
+    userData.forEach((element) => {
+        
+        let row = document.createElement("tr");
+        
+        let sno = document.createElement("td");
+        sno.innerText = i;
+        i++;
+
+        let name = document.createElement("td");
+        name.innerText = element.fname + element.lname;
+
+        let email = document.createElement("td");
+        email.innerText = element.email;
+
+        let mob = document.createElement("td");
+        mob.innerText = element.mobile;
+
+        row.append(sno,name,email,mob);
+
+        tableBody.append(row);
+
+     });
+
+}
--- adminOrders.js
+++ adminOrders.js
@@ -1,6 +1,12 @@
 let dashboard = document.getElementById("dashboard-page");
 let products = document.getElementById("products-page");
 let customers = document.getElementById("customers-page");
+let home = document.getElementById("home");
+let tableBody = document.getElementById("tbody");
+
+home.addEventListener("click",() => {
+    window.location = "./index.html";
+})
 
 dashboard.addEventListener("click",() => {
     window.location = "./dashboard.html";
@@ -11,3 +17,41 @@ products.addEventListener("click",() => {
 customers.addEventListener("click",() => {
     window.location = "./adminCustomer.html";
 })
+
+window.addEventListener("load",() => {
+    let orderData = JSON.parse(localStorage.getItem(""));
+
+    if(orderData == undefined){
+       orderData = [];
+    }
+
+    renderCustomers(orderData);
+})
+
+function renderCustomers(orderData){
+
+   tableBody.innerHTML = "";
+     
+   orderData.forEach((element) => {
+       
+       let row = document.createElement("tr");
+
+       let order = document.createElement("td");
+       order.innerText = element.orderNo;
+
+       let payMode = document.createElement("td");
+       payMode.innerText = element.pmode;
+
+       let payStatus = document.createElement("td");
+       payStatus.innerText = element.pstatus;
+
+       let amount = document.createElement("td");
+       amount.innerText = element.bamount;
+
+       row.append(order,payMode,payStatus,amount);
+
+       tableBody.append(row);
+
+    });
+
+}
